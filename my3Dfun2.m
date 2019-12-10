f05d3f798b38a66a9381e8b61c801f69690bb3b8
Lenka Galcikova <xgalcikova@stuba.sk>
2019-12-10 11:12:07 +0100

    my3Dfun2A

---
+++ my3Dfun2.m
@@ -0,0 +1,41 @@
+%% my3Dfun2
+
+%% Function domain
+
+[x,y] = meshgrid(-10:0.1:10,-10:0.1:10);
+    
+%% Function values
+
+z = sin(sqrt(x.^2+y.^2)+eps)./(sqrt(x.^2+y.^2));
+    
+%% Figure initialization
+
+figure 
+    
+%% Plotting figure
+
+surf(x,y,z)
+    
+%% Title
+
+title('z_A = sin(sqrt(x^2+y^2)+eps)/sqrt(x^2+y^2)')
+
+%% Labels
+
+xlabel('x_A')
+
+ylabel('y_A')
+
+zlabel('z_A')
+    
+%% Grid
+
+grid minor
+    
+%% Colormap
+
+colormap pink
+    
+%% Shading
+
+shading interp
